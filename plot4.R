c836416ae44c3035451ab68dde1adfe7eedd5ad8
kchalasa <kantic@hotmail.com>
2014-06-07 06:27:33 -0400

    plot4.R - R code to generate plot4.png

--- plot4.R
+++ plot4.R
@@ -17,14 +17,14 @@
 	
 	## Generate Plot4
 	
-	par(mfrow = c(2, 2))
+	par(mfrow = c(2, 2),height=480,width=480)
 	plot(as.POSIXct(paste(pc$Date,pc$Time)),pc$Global_active_power,type="l",xlab="",ylab="Global Active Power")
 	plot(as.POSIXct(paste(pc$Date,pc$Time)),pc$Voltage,type="l",xlab="datetime",ylab="Voltage")
 	plot(as.POSIXct(paste(pc$Date,pc$Time)),pc$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
 	lines(as.POSIXct(paste(pc$Date,pc$Time)),pc$Sub_metering_2,type="l",col="red")
 	lines(as.POSIXct(paste(pc$Date,pc$Time)),pc$Sub_metering_3,type="l",col="blue")
 	legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=c(1,1),pt.cex=1.25,cex=0.75)
-	plot(as.POSIXct(paste(pc$Date,pc$Time)),pc$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
+	plot(as.POSIXct(paste(pc$Date,pc$Time)),pc$Global_reactive_power,type="s",xlab="datetime",ylab="Global_reactive_power")
 	dev.copy(png, file = "plot4.png",width = 480, height = 480) ## Copy plot4 to a PNG file
 	dev.off()
 	
